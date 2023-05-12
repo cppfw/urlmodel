b630129cc8a4d854aeb8ed22b6e89c65f67a52fc
Ivan Gagis <igagis@gmail.com>
2023-05-12 13:03:39 +0300

    conan2

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -6,21 +6,19 @@ from conan.tools.layout import basic_layout
 
 class UrlmodelConan(ConanFile):
 	name = "urlmodel"
-	# NOTE: version is supposed to be specified as `--version` argument
-	#  to 'conan create' command from command line
-	# version = ""
 	license = "MIT"
 	author = "Ivan Gagis <igagis@gmail.com>"
 	url = "http://github.com/cppfw/" + name
 	description = "C++ URL model library"
 	topics = ("C++", "cross-platform")
 	settings = "os", "compiler", "build_type", "arch"
+	package_type = "library"
 	options = {"shared": [True, False], "fPIC": [True, False]}
 	default_options = {"shared": False, "fPIC": True}
 	generators = "AutotoolsDeps" # this will set CXXFLAGS etc. env vars
 
 	def requirements(self):
-		self.requires("utki/[>=1.1.192]@cppfw/main", transitive_headers=True)
+		self.requires("utki/[>=1.1.202]@cppfw/main", transitive_headers=True)
 		self.tool_requires("tst/[>=0.0.1]@cppfw/main")
 
 	def config_options(self):
@@ -52,14 +50,19 @@ class UrlmodelConan(ConanFile):
 		dst_include_dir = os.path.join(self.package_folder, "include")
 		dst_lib_dir = os.path.join(self.package_folder, "lib")
 		dst_bin_dir = os.path.join(self.package_folder, "bin")
+		
 		copy(conanfile=self, pattern="*.h",                    dst=dst_include_dir, src=src_dir,     keep_path=True)
 		copy(conanfile=self, pattern="*.hpp",                  dst=dst_include_dir, src=src_dir,     keep_path=True)
-		copy(conanfile=self, pattern="*" + self.name + ".lib", dst=dst_lib_dir,     src="",          keep_path=False)
-		copy(conanfile=self, pattern="*.dll",                  dst=dst_bin_dir,     src=src_rel_dir, keep_path=False)
-		copy(conanfile=self, pattern="*.so",                   dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
-		copy(conanfile=self, pattern="*.so.*",                 dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
-		copy(conanfile=self, pattern="*.dylib",                dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
-		copy(conanfile=self, pattern="*.a",                    dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
+
+		if self.options.shared:
+			copy(conanfile=self, pattern="*" + self.name + ".lib", dst=dst_lib_dir,     src="",          keep_path=False)
+			copy(conanfile=self, pattern="*.dll",                  dst=dst_bin_dir,     src=src_rel_dir, keep_path=False)
+			copy(conanfile=self, pattern="*.so",                   dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
+			copy(conanfile=self, pattern="*.so.*",                 dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
+			copy(conanfile=self, pattern="*.dylib",                dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
+		else:
+			copy(conanfile=self, pattern="*" + self.name + ".lib", dst=dst_lib_dir,     src="",          keep_path=False)
+			copy(conanfile=self, pattern="*.a",                    dst=dst_lib_dir,     src=src_rel_dir, keep_path=False)
 
 	def package_info(self):
 		self.cpp_info.libs = [self.name]
